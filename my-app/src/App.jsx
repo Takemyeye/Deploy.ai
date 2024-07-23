d5e7a916b09697cd87a77537bbcca1115d9da585
Takemyeye <mellovan2005@gmail.com>
2024-07-23 05:48:41 +0200

    /

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route exact path='/' element={<Landing />} /> */}        
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
