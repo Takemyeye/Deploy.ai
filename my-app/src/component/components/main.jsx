949cec307c641258ce363d7debb8e44ee5df8193
Takemyeye <mellovan2005@gmail.com>
2024-07-26 10:44:55 +0200

    \

--- my-app/src/component/components/main.jsx
+++ my-app/src/component/components/main.jsx
@@ -10,7 +10,7 @@ export function Main () {
         <div className="main">
             <div className="leftPanel">
                 <h1>Unleash the Power of AI for Your Business</h1>
-                <h3>Deploy AI models seamlessly and scale your applications with ease. Unlock new possibilities and drive innovation.</h3>
+                <h4>Deploy AI models seamlessly and scale your applications with ease. Unlock new possibilities and drive innovation.</h4>
                 <div className="btnContainer">
                     <div className="btn">Get Started</div>
                     <div className="btn">Contact Sales</div>
